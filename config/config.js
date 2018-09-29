d0bc0d558a56f09ede954e1d64cefc234930785c
igoist <493512132@qq.com>
2018-09-29 09:48:25 +0800

    feature/antd: 路由配置

--- config/config.js
+++ config/config.js
@@ -8,10 +8,22 @@ export default {
     path: '/',
     component: '../layout',
     routes: [
+      // {
+      //   path: '/',
+      //   component: 'Helloworld',
+      // },
       {
         path: 'helloworld',
         component: './HelloWorld',
       },
+      {
+        path: '/dashboard',
+        routes: [
+          { path: '/dashboard/analysis', component: 'Dashboard/Analysis' },
+          { path: '/dashboard/monitor', component: 'Dashboard/Monitor' },
+          { path: '/dashboard/workplace', component: 'Dashboard/Workplace' },
+        ]
+      }
     ]
   }],
 };
