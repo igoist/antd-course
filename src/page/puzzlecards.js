29c64a46515b580c429c932e67ef43f7c6bf7f4b
igoist <493512132@qq.com>
2018-10-01 23:28:00 +0800

    feature/antd: 在 model 中请求服务端数据

--- src/page/puzzlecards.js
+++ src/page/puzzlecards.js
@@ -12,30 +12,40 @@ const mapStateToProps = (state) => {
 };
 
 const mapDispatchToProps = dispatch => ({
+  onDidMount: () => dispatch({ type: `${namespace}/queryInitCards` }),
   onClickAdd: newCard => dispatch({ type: `${namespace}/addNewCard`, payload: newCard })
 });
 
-const PuzzleCardsPage = ({ cardList, onClickAdd }) => (
-  <div>
-    {
-      cardList.map(card => {
-        return (
-          <Card key={ card.id }>
-            <div>Q: { card.setup }</div>
-            <div>
-              <strong>A: { card.punchline }</strong>
-            </div>
-          </Card>
-        );
-      })
-    }
-    <div>
-    <Button onClick={ () => onClickAdd({
-      setup: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-      punchline: 'here we use dva',
-    }) }> 添加卡片 </Button>
-    </div>
-  </div>
-);
+class PuzzleCardsPage extends React.Component {
+  componentDidMount() {
+    this.props.onDidMount();
+  }
+
+  render() {
+    const { cardList, onClickAdd } = this.props;
+    return (
+      <div>
+        {
+          cardList.map(card => {
+            return (
+              <Card key={ card.id }>
+                <div>Q: { card.setup }</div>
+                <div>
+                  <strong>A: { card.punchline }</strong>
+                </div>
+              </Card>
+            );
+          })
+        }
+        <div>
+        <Button onClick={ () => onClickAdd({
+          setup: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+          punchline: 'here we use dva',
+        }) }> 添加卡片 </Button>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(PuzzleCardsPage);
